d2fcdb4d72ab3b73c9bc2e00a5a359e515751ba8
Eyoatam <eyoatamtamirat7@gmail.com>
2020-11-06 16:15:41 +0300

    initial commit

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue';
+
+import App from './App.vue';
+import BaseModal from './components/BaseModal.vue';
+// import router from './router'
+
+const app = createApp(App);
+
+// app.use(router);
+
+app.component('base-modal', BaseModal);
+
+app.mount('#app');
